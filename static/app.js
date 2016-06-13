008a9f927732e5dcc1854b6356fa92f25503f61f
Amos Omondi <amos.omondi@andela.com>
2016-06-13 15:25:49 +0300

    (Feature) Add configurations for Angular app

--- static/app.js
+++ static/app.js
@@ -1,5 +1,22 @@
 angular
     .module("lido", [
                         'ui.router',
-                        'ngMaterial'
-                    ]);
+                        'ngMaterial',
+                        'firebase'
+                    ])
+    .config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
+        function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+            $urlRouterProvider.otherwise("/");
+
+            $stateProvider
+                .state('login', {
+                    url: "/",
+                    templateUrl: "../templates/partials/login.html"
+                })
+                .state('lido', {
+                    url: "/lido/",
+                    templateUrl: "../templates/partials/lido.html"
+                });
+        }
+    ]);
